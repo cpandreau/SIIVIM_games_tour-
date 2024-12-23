f78bde5844dab15ffb34cf94bc2500347ae8aace
Evan VERPLAETSE <evan.verplatse@epitech.eu>
2024-12-23 22:20:43 +0100

    [PROGRESS] Marge Update

--- siivim/src/App.jsx
+++ siivim/src/App.jsx
@@ -64,51 +64,50 @@ function App() {
             </div>
           </>
         );
+        
       case 'infos':
         return (
           <div>
             <header className="header">
-            <div className="slide-bar" style={{ marginLeft: '15rem', marginTop: '1.3rem'}}></div>
-                <div className="button-container" style={{ marginLeft: '-120rem', marginTop: '1.3rem' }}> 
-                  <div className="parallelogram"></div>
-                  <button className="titre" onClick={() => setPage('home')}>ACCUEIL</button>
-                </div>
-                <div className="button-container" style={{ marginLeft: '-50px', marginTop: '1.3rem' }}>
-                  <div className="parallelogram"></div>
-                  <button className="titre" onClick={() => setPage('infos')}>INFOS</button>
-                </div>
-                <div className="button-container" style={{ marginRight: '17.8rem', marginTop: '1.3rem'}}>
-                  <div className="parallelogram"></div>
-                  <button className="titre">CLASSEMENT</button>
-                </div>
-             <img className="background2" src='../image/Fortnite2.webp' alt="Background"/>
+              <div className="slide-bar" style={{ marginLeft: '15rem', marginTop: '1.3rem' }}></div>
+              <div className="button-container" style={{ marginLeft: '-120rem', marginTop: '1.3rem' }}>
+                <div className="parallelogram"></div>
+                <button className="titre" onClick={() => setPage('home')}>ACCUEIL</button>
+              </div>
+              <div className="button-container" style={{marginTop: '1.3rem'}}>
+                <div className="parallelogram"></div>
+                <button className="titre" onClick={() => setPage('infos')}>INFOS</button>
+              </div>
+              <div className="button-container" style={{ marginRight: '17.8rem', marginTop: '1.3rem' }}>
+                <div className="parallelogram"></div>
+                <button className="titre">CLASSEMENT</button>
+              </div>
+              <img className="background2" src='../image/Fortnite2.webp' alt="Background" />
             </header>
-            <div className="info-content">
-              <div className= "parallelogram2"></div>
-              <h1 className="INSTRUCTIONS">Instructions</h1>
-              <img className="kevin" src='../image/kevin.webp' alt="Image"/>
+            <div className="info-content" style={{marginTop: '2rem' }}>
+              <div className="parallelogram2" style={{ marginLeft: '0rem', marginTop: '2rem' }}></div>
+              <h1 className="INSTRUCTIONS" style={{ marginTop: '-29.5rem', marginLeft: '-60rem' }}>Instructions</h1>
+              <img className="kevin" src='../image/kevin.webp' style={{marginLeft: '-22.5rem', marginTop: '2rem'}} alt="Image" />
               <div className="case">Organisé par la Ville de Nevers, ce tournoi se déroule du 8 février 2025 au 9 février 2025 sur Fortnite.</div>
-              <a className="Regl" href="/ppt/reglement.pdf" download>Télécharger le réglement du tournoi ici.</a>
-            <h2>Conditions d'éligibilité</h2>
-            <p>Ouvert à toutes les équipes et joueurs respectant les conditions d'éligibilité.
-            Interdit aux employés et agents de Epic Games ou membres du Comité d'organisation et leur famille immédiate.</p>
-            <p>Au moins un joueur de l'équipe doit être domicilié dans une ville éligible. </p>
-            <p>Les joueurs doivent avoir un compte Epic Games et Fortnite valide</p>
-            <p>Les joueurs ne peuvent participer que dans une seule équipe pendant toute la durée du tournoi.</p>
+              <a className="Regl" style={{ marginLeft: '-60rem', marginTop: '10rem'}} href="/ppt/reglement.pdf" download>Télécharger le réglement du tournoi ici.</a>
+              <h2 style={{ marginTop: '25rem' , marginLeft: '-60rem'}}>Conditions d'éligibilité</h2>
+              <p>Ouvert à toutes les équipes et joueurs respectant les conditions d'éligibilité.
+                Interdit aux employés et agents de Epic Games ou membres du Comité d'organisation et leur famille immédiate.</p>
+              <p>Au moins un joueur de l'équipe doit être domicilié dans une ville éligible. </p>
+              <p>Les joueurs doivent avoir un compte Epic Games et Fortnite valide</p>
+              <p>Les joueurs ne peuvent participer que dans une seule équipe pendant toute la durée du tournoi.</p>
               <p>Tous les participants doivent respecter l'esprit de fair-play et les règles du jeu.</p>
               <p>Les inscriptions doivent être complètes et conformes aux exigences avant la date limite fixée par l'organisation.</p>
               <p>Les participants doivent s'assurer de la stabilité de leur connexion internet pour les phases en ligne.</p>
               <p>Les décisions de l'arbitrage pendant le tournoi sont finales et sans appel.</p>
               <p>Les participants doivent être disponibles pour les phases en ligne.</p>
-              <h2>Âge minimum requis</h2>
+              <h2 style={{ marginTop: '2rem' }}>Âge minimum requis</h2>
               <p>Les joueurs doivent avoir au moins 15 ans à la date du 6 janvier 2025, les joueurs de 15 à 17 ans doivent fournir une authorisation parentale lors de l'inscriptions</p>
-              <h2>Calendrier des parties</h2>
+              <h2 style={{ marginTop: '2rem' }}>Calendrier des parties</h2>
               <p>Qualification: Samedi 8 février 2025</p>
               <p>Finale: Dimanche 9 février 2025</p>
-              <h2>Dotations</h2>
+              <h2 style={{ marginTop: '2rem' }}>Dotations</h2>
               <p>1er Duo 600€</p>
-              <p>2ème Duo 300€</p>
-              <p>3ème Duo 100€</p>
             </div>
           </div>
         );
