280987a87052483eff0300d1d7bd0be750d58ecd
Titouan Beaurepaire <titouan-luigi-roland.beaurepaire@epitech.eu>
2025-01-09 15:15:54 +0100

    [footer]

---
+++ siivim/src/footer.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./Header.css";
+import "./footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-left">
+        <a href="https://www.instagram.com/villedenevers/?hl=fr" target="_blank" rel="noopener noreferrer">
+          <img src="image/insta.png" alt="Instagram" className="footer-icon" />
+        </a>
+        <a href="https://discord.gg/g9unyDfyfG" target="_blank" rel="noopener noreferrer">
+          <img src="image/discord.png" alt="Discord" className="footer-icon" />
+        </a>
+        <a href="https://www.tiktok.com/@villedenevers" target="_blank" rel="noopener noreferrer">
+          <img src="image/tiktok.png" alt="TikTok" className="footer-icon" />
+        </a>
+      </div>
+      <div className="footer-center">
+        <p>Nevers n'est pas lié à Epic Games</p>
+      </div>
+      <div className="footer-right">
+        <p>Evan Verplaetse et Titouan Beaurepaire</p>
+      </div>
+    </footer>
+  );
+};
+export default Footer;
+
