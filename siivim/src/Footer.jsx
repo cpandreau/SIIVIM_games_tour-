68753609ba7de69a67ed3892a3f35310da2d5f2e
Erreur-92 <evan.verplaetse@epitech.eu>
2025-01-14 11:27:18 +0100

    [NAV REPONSIVE]

--- siivim/src/Footer.jsx
+++ siivim/src/Footer.jsx
@@ -5,7 +5,13 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <footer className="footer">
-      <div className="footer-left">
+      <div className="footer-center">
+        <p>Nevers n'est pas lié à Epic Games</p>
+      </div>
+      <div className="footer-right">
+        <p>Evan Verplaetse et Titouan Beaurepaire</p>
+      </div>
+      <div className="footer-icons" style={{ display: "flex", flexDirection: "column" }}>
         <a href="https://www.instagram.com/villedenevers/?hl=fr" target="_blank" rel="noopener noreferrer">
           <img src="image/insta.png" alt="Instagram" className="footer-icon" />
         </a>
@@ -15,12 +21,6 @@ const Footer = () => {
         <a href="https://www.tiktok.com/@villedenevers" target="_blank" rel="noopener noreferrer">
           <img src="image/tiktok.png" alt="TikTok" className="footer-icon" />
         </a>
-      </div>
-      <div className="footer-center">
-        <p>Nevers n'est pas lié à Epic Games</p>
-      </div>
-      <div className="footer-right">
-        <p>Evan Verplaetse et Titouan Beaurepaire</p>
       </div>
     </footer>
   );
